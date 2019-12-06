60a55bfaf506f7b176c074f225cde146e299398c
Fugarli <sl4654@columbia.edu>
2019-12-06 18:57:42 -0500

    Code cleaning - remove unnecessry parts

--- final project/mysite/map/views.py
+++ final project/mysite/map/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render
 from sightings.models import Squirrel
 
 
--- final project/mysite/sightings/views.py
+++ final project/mysite/sightings/views.py
@@ -1,29 +1,17 @@
 from .models import Squirrel
 from django.db.models import Max,Count,Min
 from .forms import SquirrelForm
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.shortcuts import redirect
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-
+from django.shortcuts import render,redirect
 
 def list(request):
     sightings = Squirrel.objects.all()[:125]
     return render(request,'sightings/list.html',{'sightings':sightings})
-
-# def delete(request,idd):
-#     obj_d= Squirrel.objects.filter(usi=idd)
-#     obj_d.delete()
-#     return redirect('/sightings/')
 
 def stats(request):
     data = Squirrel.objects.all()
     longitude_m = data.aggregate(longitude_max = Max('longitude'))
     latitude_m = data.aggregate(latitude_min = Min('latitude'))
     date_m = data.aggregate(date_max = Max('date'))
-#   color_c = data.values_list('pfc').annotate(Count('pfc'))
-#   age_c = data.values_list('age').annotate(Count('age'))
     pfc_c = data.values('pfc').annotate(Count('pfc')).distinct()
     age_c=data.values('age').annotate(Count('age')).distinct()
     
